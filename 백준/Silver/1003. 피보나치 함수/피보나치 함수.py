e1904a4dd7245336800e7cdecee49eb98740462b
최현우 <118657735+Wevisj@users.noreply.github.com>
2023-01-19 03:49:41 +0900

    [Silver III] Title: 피보나치 함수, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,16 +1,17 @@
-fibo_count_zero = {}
-fibo_count_one = {}
+import sys
 
-fibo_count_zero[0] = 1
-fibo_count_one[0] = 0
+input = sys.stdin.readline
 
-fibo_count_zero[1] = 0
-fibo_count_one[1] = 1
+zero = [0 for _ in range(41)]
+one = [0 for _ in range(41)]
+
+zero[0], zero[1] = 1, 0
+one[0], one[1] = 0, 1
 
 for i in range(2, 41):
-    fibo_count_zero[i] = fibo_count_zero[i-1] + fibo_count_zero[i-2]
-    fibo_count_one[i] = fibo_count_one[i-1] + fibo_count_one[i-2]
+    zero[i] = zero[i - 1] + zero[i - 2]
+    one[i] = one[i - 1] + one[i - 2]
 
 for _ in range(int(input())):
     n = int(input())
-    print(fibo_count_zero[n], fibo_count_one[n])
+    print(zero[n], one[n])
