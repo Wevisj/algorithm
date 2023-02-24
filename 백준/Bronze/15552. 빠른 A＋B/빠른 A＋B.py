6460544cd1f44402e5625ba2d67574dff43ecf01
최현우 <118657735+Wevisj@users.noreply.github.com>
2023-02-24 18:36:09 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1100 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.py
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.py
@@ -1,9 +1,10 @@
 import sys
 
 input = sys.stdin.readline
+print = sys.stdout.write
 
 T = int(input())
 
 for _ in range(T):
-    a, b = map(int, input().split())
-    print(a + b)
+    a, b = map(int, input().rstrip().split())
+    print(str(a + b) + "\n")
