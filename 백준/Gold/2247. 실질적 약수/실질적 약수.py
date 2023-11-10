e743c7f6d857689039796417b59a92ffc868260e
최현우 <118657735+Wevisj@users.noreply.github.com>
2023-11-11 01:06:20 +0900

    [Gold V] Title: 실질적 약수, Time: 52 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Gold/2247. 실질적 약수/실질적 약수.py
+++ 백준/Gold/2247. 실질적 약수/실질적 약수.py
@@ -1,9 +1,13 @@
 #  실질적 약수
 
+import math
+
 n = int(input())
 res = 0
 
-for i in range(2, n):
-    res += (n // i - 1) * i
+for i in range(2, int(math.sqrt(n)) + 1):
+    k = n // i
+    res += i * (k - i + 1) + (k - i) * (k + i + 1) // 2
+    res %= 1000000
 
-print(res % 1000000)
+print(res)
