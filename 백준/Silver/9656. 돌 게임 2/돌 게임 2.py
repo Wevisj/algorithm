e41ddb0a11e96f664eb5e09f945fc0a7c26e1547
최현우 <118657735+Wevisj@users.noreply.github.com>
2023-11-11 23:26:57 +0900

    [Silver IV] Title: 돌 게임 2, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9656. 돌 게임 2/돌 게임 2.py
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.py
@@ -1,8 +1,20 @@
-#  돌 게임 2
+#  돌 게임 2 / DP
 
 n = int(input())
 
+dp = [0 for i in range(n + 2)]
+dp[0] = 0
+dp[1] = 1
+dp[2] = 2
+
+for i in range(3, n+1):
+    dp[i] = min(dp[i-1], dp[i-3]) + 1
+
+print('SK' if dp[n] % 2 == 0 else 'CY')
+
+''' n만 보고 계산했을 때
 if n % 2 == 0:
     print('SK')
 else:
     print('CY')
+'''
