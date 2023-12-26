68982bcc4db554701e8f26660d699d7b88ed57b7
최현우 <118657735+Wevisj@users.noreply.github.com>
2023-12-26 22:29:39 +0900

    [Silver IV] Title: Fire on Field, Time: 588 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/17968. Fire on Field/Fire on Field.py
+++ 백준/Silver/17968. Fire on Field/Fire on Field.py
@@ -8,7 +8,6 @@ dp[1] = 1
 
 for i in range(2, n+1):
     while True:
-        flag = False
         dp[i] += 1
         for k in range(1, i//2 + 1):
             if dp[i] - dp[i-k] == dp[i-k] - dp[i - 2*k]:
